83afe913f4fc886871fdfaa101b50ec92b5055be
fraimparato <fra.imparato20@gmail.com>
2022-06-25 13:43:26 +0200

    added treesitter-context plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -48,4 +48,22 @@ return require('packer').startup(function()
         config = get_setup("treesitter")
     }
 
+    use { 
+        'nvim-treesitter/nvim-treesitter-context',
+        require'treesitter-context'.setup{
+            enable = true,
+            max_lines = 0,
+            trim_scope = 'outer',
+            patterns = {
+                default = {
+                    'class',
+                    'function',
+                    'method',
+                },
+            },
+            zindex = 20,
+            mode = 'cursor'
+        }
+    }
+
 end)
