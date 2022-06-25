a4dde89bebc9ad6833fe17f5c37481241a788846
fraimparato <fra.imparato20@gmail.com>
2022-06-25 15:15:25 +0200

    added comments in 'init.lua', for better readability

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,6 +9,7 @@ return require('packer').startup(function()
     -- Packer
     use { 'wbthomason/packer.nvim' }
 
+    -- Lualine, Nvim-tree, Nvim-web-devicons
     use {
         "nvim-lualine/lualine.nvim",
         config = get_setup("lualine"),
@@ -21,12 +22,12 @@ return require('packer').startup(function()
         }
     }
 
+    -- Telescope
     use {
         'nvim-telescope/telescope.nvim',
         requires = { 'nvim-lua/plenary.nvim' },
         --config = get_setup("telescope")
     }
-   
     use {
         'nvim-telescope/telescope-fzf-native.nvim',
        -- config = get_setup("telescope_fzf_native"),
@@ -42,12 +43,14 @@ return require('packer').startup(function()
         require('onedark').load()
     }
 
+    -- Treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
         config = get_setup("treesitter")
     }
 
+    -- Treesitter context (upperline indicating the function in which you're in)
     use { 
         'nvim-treesitter/nvim-treesitter-context',
         require'treesitter-context'.setup{
